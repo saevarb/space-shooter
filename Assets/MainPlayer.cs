b2106176c15edcd05bb9cab4caa48b0135136ce7
Sævar Berg <icedane@gmail.com>
2018-07-07 23:01:53 +0200

    Initial commit

---
+++ Assets/MainPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour {
+    void Start () {
+        int a = 3;
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+}
