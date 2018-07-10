5f72b293866d57bcc5d60f910ba5537f483fab95
Sævar Berg <icedane@gmail.com>
2018-07-10 20:34:37 +0200

    Added HpDisplay behavior

---
+++ Assets/Behaviors/HpDisplay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpDisplay : MonoBehaviour {
+
+    private Text hpText;
+    private Killable killable;
+	// Use this for initialization
+	void Awake () {
+        GameObject canvasObject = GameObject.Find("dmgCanvas");
+        Canvas dmgCanvas = canvasObject.GetComponent<Canvas>();
+
+        killable = GetComponent<Killable>();
+
+        if(killable != null) {
+            GameObject tObj = new GameObject();
+
+            tObj.transform.SetParent(dmgCanvas.transform);
+
+            hpText = tObj.AddComponent<Text>();
+
+            UpdateText();
+
+            hpText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+            hpText.rectTransform.sizeDelta = new Vector2(10, 10);
+            hpText.horizontalOverflow = HorizontalWrapMode.Overflow;
+            hpText.verticalOverflow = VerticalWrapMode.Overflow;
+        }
+    }
+
+    private void UpdateText() {
+        if(killable != null) {
+            hpText.gameObject.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(-.15f, .08f, 0));
+            hpText.text = killable.hp.ToString("0.0");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateText();
+	}
+    private void OnDestroy() {
+        if(killable != null) {
+            Destroy(hpText.gameObject);
+        }
+    }
+}
--- Assets/Prefabs/Asteroid.cs
+++ Assets/Prefabs/Asteroid.cs
@@ -5,32 +5,13 @@ using UnityEngine.UI;
 
 public class Asteroid : MonoBehaviour {
     private GameObject playerShip;
-    private Text hpText;
-    private Killable killable;
-
     // Use this for initialization
     void Start () {
         playerShip = GameObject.Find("mainPlayer");
-        GameObject canvasObject = GameObject.Find("dmgCanvas");
-        Canvas dmgCanvas = canvasObject.GetComponent<Canvas>();
-
-        GameObject tObj = new GameObject();
-        tObj.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(-.15f, .08f, 0));
-        tObj.transform.SetParent(dmgCanvas.transform);
-        killable = GetComponent<Killable>();
-
-        hpText = tObj.AddComponent<Text>();
-        hpText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-        hpText.rectTransform.sizeDelta = new Vector2(10, 10);
-        hpText.horizontalOverflow = HorizontalWrapMode.Overflow;
-        hpText.verticalOverflow = VerticalWrapMode.Overflow;
-
-        hpText.text = killable.hp.ToString("0.0");
     }
 
     // Update is called once per frame
     void Update () {
-        hpText.text = killable.hp.ToString("0.0");
     }
 
     void OnMouseDown() {
@@ -39,7 +20,6 @@ public class Asteroid : MonoBehaviour {
     }
 
     private void OnDestroy() {
-        Destroy(hpText); 
     }
 
 }
