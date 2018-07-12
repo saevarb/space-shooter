e5dd297cff3c42d4f4f2e4baecfbea8c53868100
Sævar Berg <icedane@gmail.com>
2018-07-13 01:26:47 +0200

    Fixed stupid bug in pathfinding

--- Assets/Behaviors/Pathfinder.cs
+++ Assets/Behaviors/Pathfinder.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using UnityEngine;
 using MoreLinq;
 
+[RequireComponent(typeof(Collider2D))]
 public class Pathfinder : MonoBehaviour {
     [Range(1, 60)]
     public int neighborCount = 5;
@@ -13,15 +14,26 @@ public class Pathfinder : MonoBehaviour {
     public bool debug = false;
 
     private HashSet<Vector3> closedSet;
-    private List<Vector3> openSet;
+    private SortedList<float, Vector3> openSet;
     private Dictionary<Vector3, Vector3> cameFrom;
     private Dictionary<Vector3, float> gScore;
     private Dictionary<Vector3, float> fScore;
     private Debugger debugger;
+
+    public class DuplicateKeyComparer<TKey> : IComparer<TKey> where TKey : IComparable {
+        public int Compare(TKey x, TKey y) {
+            int result = x.CompareTo(y);
+
+            if (result == 0)
+                return 1;   // Handle equality as beeing greater
+            else
+                return result;
+        }
+    }
     // Use this for initialization
     private List<Vector3> GenerateNeighbors(Vector3 cur) {
         List<Vector3> neighbors = new List<Vector3>();
-        for(float angle = 0; angle <= Mathf.PI * 2; angle += 2 * Mathf.PI / neighborCount) {
+        for (float angle = 0; angle <= Mathf.PI * 2; angle += 2 * Mathf.PI / neighborCount) {
             Vector3 v = stepSize * new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
             //Debug.DrawRay(cur, v, Color.green);
             neighbors.Add(cur + v);
@@ -29,15 +41,24 @@ public class Pathfinder : MonoBehaviour {
         return neighbors;
     }
 
+    private Queue<Vector3> ReconstructPath(Vector3 current) {
+        Stack<Vector3> pathStack = new Stack<Vector3>(); ;
+        do {
+            pathStack.Push(current);
+            current = (Vector3)cameFrom[current];
+        } while (cameFrom.ContainsKey(current));
+        return new Queue<Vector3>(pathStack);
+    }
+
     private float GetScore(Dictionary<Vector3, float> scoreTable, Vector3 key) {
-        if(scoreTable.ContainsKey(key)) {
+        if (scoreTable.ContainsKey(key)) {
             return scoreTable[key];
         } else {
-            return float.MaxValue/2;
+            return float.MaxValue / 2;
         }
     }
 
-    public List<Vector3> FindPath(Vector3 dest) {
+    public Queue<Vector3> FindPath(Vector3 dest) {
         closedSet.Clear();
         openSet.Clear();
         cameFrom.Clear();
@@ -45,89 +66,77 @@ public class Pathfinder : MonoBehaviour {
         fScore.Clear();
         var start = transform.position;
 
-        openSet.Add(start);
         gScore.Add(start, 0);
         fScore.Add(start, Vector3.SqrMagnitude(start - dest));
+        openSet.Add(GetScore(fScore, start), start);
 
         Vector3 current;
         debugger.Clear();
-        while(openSet.Count != 0) {
-            current = openSet.MinBy(x => GetScore(fScore, x)).First();
-            if(Vector3.SqrMagnitude(current - dest) <= 1) {
+        int explored = 0;
+        while (openSet.Count != 0) {
+            current = openSet.First().Value;
+            openSet.RemoveAt(0);
+            explored++;
+            if (Vector3.SqrMagnitude(current - dest) <= 1) {
+                Debug.Log("Nodes explored: " + explored);
                 return ReconstructPath(current);
             }
-            openSet.Remove(current);
             closedSet.Add(current);
 
-
             List<Vector3> neighbors = GenerateNeighbors(current);
-            if(debug)
-                debugger.DrawPoints(neighbors, Color.green);
+            if (debug) {
+                //debugger.DrawPoints(neighbors, Color.white);
+                debugger.DrawPoint(current, Color.black);
+            }
             foreach (Vector3 neighbor in neighbors) {
-                if(closedSet.Contains(neighbor)) {
+                if (closedSet.Contains(neighbor)) {
                     continue;
                 }
                 var neighDist = Vector3.SqrMagnitude(current - neighbor);
+                var neighHeading = neighbor - current;
 
                 var filter = new ContactFilter2D();
                 filter.NoFilter();
 
-                RaycastHit2D[] results = new RaycastHit2D[100];
-                int hitCount = Physics2D.Raycast(new Vector2(current.x, current.y), new Vector2(neighbor.x, neighbor.y), filter, results, neighDist);
-                if(hitCount > 0) {
-                    bool skipPoint = false;
-                    for(int i = 0; i < hitCount; i++) {
-                        if (results[i].collider.tag != "Drone") {
-                            closedSet.Add(neighbor);
-                            skipPoint = true;
-                            break;
-                        }
-                    }
-                    if (skipPoint)
-                        continue;
+                //int hitCount = Physics2D.Raycast(new Vector2(current.x, current.y), new Vector2(neighbor.x, neighbor.y), filter, results, neighDist);
+                var foo = GetComponent<CircleCollider2D>();
+                RaycastHit2D hit = Physics2D.CircleCast(new Vector2(current.x, current.y), foo.radius, neighHeading, neighDist);
+                if (hit && hit.collider.gameObject != this.gameObject) {
+                    closedSet.Add(neighbor);
+                    continue;
                 }
-                if(!openSet.Contains(neighbor)) {
-                    openSet.Add(neighbor);
+                if (openSet.ContainsValue(neighbor)) {
+                    continue;
                 }
                 float tentativeGScore = GetScore(gScore, current);
-                tentativeGScore += neighDist; 
+                tentativeGScore += neighDist;
                 if (tentativeGScore >= GetScore(gScore, neighbor)) {
                     continue;
                 }
                 cameFrom[neighbor] = current;
                 gScore[neighbor] = tentativeGScore;
                 fScore[neighbor] = (float)gScore[neighbor] + Vector3.SqrMagnitude(neighbor - dest);
+                openSet.Add(GetScore(fScore, neighbor), neighbor);
             }
         }
         return null;
     }
 
-    private List<Vector3> ReconstructPath(Vector3 current) {
-        List<Vector3> path = new List<Vector3>();
-        path.Add(current);
-        while(cameFrom.ContainsKey(current)) {
-            current = (Vector3)cameFrom[current];
-            path.Add(current);
-        }
-        path.Reverse();
-        return path;
-    }
-
     private void Awake() {
         closedSet = new HashSet<Vector3>();
-        openSet = new List<Vector3>(); 
+        openSet = new SortedList<float, Vector3>(new DuplicateKeyComparer<float>()); 
         cameFrom = new Dictionary<Vector3, Vector3>();
         gScore = new Dictionary<Vector3, float>();
         fScore = new Dictionary<Vector3, float>();
         debugger = GetComponent<Debugger>();
-        if(debugger == null) {
+        if (debugger == null) {
             debugger = gameObject.AddComponent<Debugger>();
         }
     }
-    void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
 }
